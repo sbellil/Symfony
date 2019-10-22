193a418af8d7c62c509f935d57180e4af7b079ab
sbellil <zinabellil@gmail.com>
2019-10-22 16:33:07 +0200

    correction post

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,25 +3,19 @@
 
 namespace App\Controller;
 
-//use App\Form\ContactType;
-//use App\Entity\Contact;
-//use App\Notification\NotificationEmail;
 use App\Entity\Contact;
 use App\Form\ContactType;
-use FOS\RestBundle\View\View;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Notification\NotificationEmail;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Entity\Departement;
-use Doctrine\ORM\EntityManager;
-use FOS\RestBundle\Controller\Annotations\Get;
-use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations as Rest;
-class ContactController extends abstractController
+
+class ContactController extends AbstractFOSRestController implements ClassResourceInterface
 {
     /**
      * @Rest\View()
@@ -47,42 +41,57 @@ class ContactController extends abstractController
         return new JsonResponse($formatted);
     }
 
-        /**
-         * @Rest\View(statusCode=Response::HTTP_CREATED)
-         * @Rest\Post("/contact")
-         * @return View
-         */
-        public function postContactAction(Request $request)
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    ) {
+        $this->entityManager = $entityManager;
+    }
+/**
+ * @Rest\Post("/contact")
+ */
+    public function postAction(Request $request,NotificationEmail $notification)
     {
-        $contact = new Contact();
-        $form = $this->createForm(ContactType::class, $contact);
+        $form = $this->createForm(ContactType::class,new Contact());
 
         $form->submit($request->request->all());
 
+        if (false === $form->isValid()) {
 
 
 
-        if ($form-> isValid()) {
-            $em = $this->get('doctrine.orm.entity_manager');
-            $em->persist($contact);
-            $em->flush();
-            return $contact;
-
-            //récupération des données
-            $departement = $contact->getDepartement();
-            $email_responsable = $departement->getEmailResponsable();
-            $nom = $contact->getNom();
-            $prenom = $contact ->getPrenom();
-            $msg = $contact ->getMessage();
-            $mail = $contact ->getMail();
-            //envoyer un le mail
-            $notification->sendEmail($email_responsable,$msg,$nom,$prenom,$mail);
-
-
-        } else {
-            return $form;
+            return $this->handleView(
+                $this->view($form)
+            );
         }
 
+        $this->entityManager->persist($form->getData());
+        $this->entityManager->flush();
+
+        $contact = $form->getData();
+        $departement = $contact->getDepartement();
+        $email_responsable = $departement->getEmailResponsable();
+        $nom = $contact->getNom();
+        $prenom = $contact ->getPrenom();
+        $msg = $contact ->getMessage();
+        $mail = $contact ->getMail();
+        //envoyer un le mail
+        $notification->sendEmail($email_responsable,$msg,$nom,$prenom,$mail);
+        
+
+        return $this->handleView(
+            $this->view(
+                [
+                    'status' => 'ok',
+                ],
+                Response::HTTP_CREATED
+            )
+        );
     }
 
 
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -33,6 +33,7 @@ class ContactType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Contact::class,
+            'csrf_protection' => false,
         ]);
     }
 }
